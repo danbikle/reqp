a95603d73a0b3c4535eaf430eea646170f2d27bc
Dan Bikle <danbikle@outlook.com>
2017-08-06 16:58:07 -0700

    more

--- py/reqp.py
+++ py/reqp.py
@@ -15,4 +15,13 @@ import sys
 import time
 import pdb
 
+# I should use tkrlist.txt drive a loop
+
+with open('/home/reqp/reqp/tkrlist.txt') as fh:
+  tkrlist_s = fh.read()
+  tkrlist_l = tkrlist_s.split()
+  for tkr in tkrlist_l:
+    pdb.set_trace()
+    tkr
+  
 'bye'
