0abcfeb91dc9c6528547149f671ad90af738f8ac
Dan Bikle <danbikle@outlook.com>
2017-08-06 17:51:21 -0700

    more

--- py/reqp.py
+++ py/reqp.py
@@ -17,8 +17,11 @@ import pdb
 
 # I should ensure the output folders exist
 outdirh = '/home/reqp/html/'
-outdirc = '/home/reqp/csv/'
-os.system('mkdir -p '+outdirh+' '+outdirc)
+os.system('mkdir -p '+outdirh)
+
+csv_types_l = ['div','history','split']
+for type_s in csv_types_l:
+  os.system('mkdir -p /home/reqp/csv/'+type_s)
 
 # I should prepare to talk to Yahoo:
 yahoo_s      = 'https://finance.yahoo.com/quote/'
@@ -26,9 +29,13 @@ user_agent_s = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like 
 headers_d    = {'User-Agent': user_agent_s}
 ycsv_s       = 'https://query1.finance.yahoo.com/v7/finance/download/'
 nowutime_s   = datetime.datetime.now().strftime("%s")
-params_s     = '?period1=-631123200&period2='+nowutime_s+'&interval=1d&events=history&crumb='
+history_s    = '?period1=-631123200&period2='+nowutime_s+'&interval=1d&events=history&crumb='
+params_s     = '?period1=-631123200&period2='+nowutime_s+'&interval=1d&events='
 
 # I should use tkrlist.txt drive a loop
+
+# I get csv from URL like this:
+# https://query1.finance.yahoo.com/v7/finance/download/AA?period1=1492462308&period2=1495054308&interval=1d&events=history&crumb=pYheK5rafih
 
 with open('/home/reqp/reqp/tkrlist.txt') as fh:
   tkrlist_s   = fh.read()
@@ -40,18 +47,18 @@ with open('/home/reqp/reqp/tkrlist.txt') as fh:
       html_s  = tkr_r.content.decode("utf-8")
       with open(outdirh+tkr+'.html','w') as fh:
         fh.write(html_s)
-      pdb.set_trace()
       pattern_re = r'(CrumbStore":{"crumb":")(.+?")'
       pattern_ma = re.search(pattern_re, html_s)
       crumb_s    = pattern_ma[2].replace('"','') # erase " on end of crumb
-      csvurl_s   = ycsv_s+tkr+params_s+crumb_s
-      # yahoo server needs time to remember the cookie-crumb-pair it just served:
-      time.sleep(5)
-      csv_r      = ssn.get(csvurl_s)
-      csv_s      = csv_r.content.decode("utf-8")
-      csv_status_i = csv_r.status_code
-      # I should write the csv_s to csv file:
-      with open(outdirc+tkr+'.csv','w') as fh:
-        fh.write(csv_s)
+      for type_s in csv_types_l:
+        csvurl_s   = ycsv_s+tkr+params_s+type_s+'&crumb='+crumb_s
+        # yahoo server needs time to remember the cookie-crumb-pair it just served:
+        time.sleep(5)
+        csv_r      = ssn.get(csvurl_s)
+        csv_s      = csv_r.content.decode("utf-8")
+        csv_status_i = csv_r.status_code
+        # I should write the csv_s to csv file:
+        with open('/home/reqp/csv/'+type_s+'/'+tkr+'.csv','w') as fh:
+          fh.write(csv_s)
         
 'bye'
