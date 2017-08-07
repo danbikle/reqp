869b89dcb09809bd54b9667a65495d8df3b00463
Dan Bikle <danbikle@outlook.com>
2017-08-07 01:07:07 -0700

    more

--- fflask.py
+++ fflask.py
@@ -8,7 +8,17 @@ Demo:
 """
 import os
 from   flask import Flask
+from   flask import send_from_directory
 application = Flask(__name__)
+
+@application.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(application.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+@application.route("/")
+def index():
+    return send_from_directory(os.path.join(application.root_path, 'static'),'index.html')
                                
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5047))
